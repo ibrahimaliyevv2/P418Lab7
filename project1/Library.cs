25c40328b84fdb156b887c5cc2e3be0610fc4d96
ibrahimaliyevv2 <7caucb@code.edu.az>
2023-09-28 22:31:57 +0400

    task

---
+++ project1/Library.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace project1
+{
+    internal class Library
+    {
+        public Book[] books = new Book[0];
+
+        public void AddBook(string title, string author, int year)
+        {
+            Book newBook = new Book(title, author, year);
+            Array.Resize(ref books, books.Length + 1);
+            books[books.Length - 1] = newBook;
+        }
+    }
+}
+
---
+++ project1/Program.cs
@@ -0,0 +1,20 @@
+﻿using project1;
+
+Library library1 = new Library();
+
+    library1.AddBook("Danabas kendinin ehvalatlari", "Celil Memmedquluzade", 1960);
+    library1.AddBook("Ferrarisini satan rahib", "Robin Sharm", 2006);
+    library1.AddBook("Yalnizliqdan sizofreniyaya", "OAR", 1999);
+    library1.AddBook("Xeyanet", "Elxan Elatli", 2008);
+    library1.AddBook("Menim sirin portagal agacim", "Joseph Maurino", 2013);
+    library1.AddBook("Antichrist", "Nitsche", 1895);
+
+    Console.WriteLine("Menim lib1 kitablarim:");
+    Console.WriteLine("                                    ");
+
+
+    foreach (Book book in library1.books)
+    {
+        Console.WriteLine($"{book.Title} by {book.Author} in ({book.Year})");
+    }
+
